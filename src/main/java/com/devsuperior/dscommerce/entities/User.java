9727b192a43d09e33c13aac3c03e3c5d87022ae3
joaomarcelo108 <j.marcelo.medeiros1999@gmail.com>
2025-12-12 20:21:43 -0300

    Category Product

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -12,10 +12,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private String phone;
+
     private LocalDate LocalDate;
+
     private String password;
 
     @OneToMany(mappedBy = "client")
