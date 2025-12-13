a85161035f1ba762be35b067806f0199081e9ab8
joaomarcelo108 <j.marcelo.medeiros1999@gmail.com>
2025-12-13 04:20:20 -0300

    GET product by id

--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -10,18 +10,13 @@ import java.util.Set;
 @Entity
 @Table(name = "tb_product")
 public class Product {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
     @Column(columnDefinition = "TEXT")
     private String description;
-
     private Double price;
-
     private String imgUrl;
 
     @ManyToMany
